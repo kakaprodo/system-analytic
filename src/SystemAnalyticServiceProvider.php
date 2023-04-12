4cdaf7357b337cc2803c4299062fbf567788a061
promesse <promskakaprodo45@gmail.com>
2023-04-12 17:09:12 +0200

    Config files: added a command to publish the package config file

--- src/SystemAnalyticServiceProvider.php
+++ src/SystemAnalyticServiceProvider.php
@@ -4,6 +4,7 @@ namespace Kakaprodo\SystemAnalytic;
 
 use Exception;
 use Illuminate\Support\ServiceProvider;
+use Kakaprodo\SystemAnalytic\Console\InstallAnalyticPackage;
 
 class SystemAnalyticServiceProvider extends ServiceProvider
 {
@@ -24,9 +25,23 @@ class SystemAnalyticServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->registerCommands();
+
         $this->checkForRequiredPackages();
 
         $this->stackToPublish();
+    }
+
+    /**
+     * Register the command if we are using the application via the CLI
+     */
+    protected function registerCommands()
+    {
+        if (!$this->app->runningInConsole()) return;
+
+        $this->commands([
+            InstallAnalyticPackage::class,
+        ]);
     }
 
     protected function checkForRequiredPackages()
@@ -34,16 +49,12 @@ class SystemAnalyticServiceProvider extends ServiceProvider
         if (!class_exists('Kakaprodo\CustomData\CustomData')) {
             throw new Exception('The system analytic package requires the latest version of kakaprodo/custom-data');
         }
-
-        if (!class_exists('Maatwebsite\Excel\Excel')) {
-            throw new Exception('The system analytic package requires the latest version of maatwebsite/excel');
-        }
     }
 
     public function stackToPublish()
     {
         $this->publishes([
             __DIR__ . '/config/system-analytic.php' => config_path('system-analytic.php'),
-        ]);
+        ], 'config');
     }
 }
