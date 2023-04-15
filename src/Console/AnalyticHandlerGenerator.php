aa6ee3a6ff9eabcb0d0d2702c910de3b7e937671
promesse <promskakaprodo45@gmail.com>
2023-04-15 13:42:03 +0200

    Generate  handler(stub): added skeleton for all supported handler types

--- src/Console/AnalyticHandlerGenerator.php
+++ src/Console/AnalyticHandlerGenerator.php
@@ -2,8 +2,9 @@
 
 namespace Kakaprodo\SystemAnalytic\Console;
 
-use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
+use Illuminate\Console\GeneratorCommand;
 use Kakaprodo\SystemAnalytic\Utilities\Util;
 
 class AnalyticHandlerGenerator extends GeneratorCommand
@@ -13,7 +14,7 @@ class AnalyticHandlerGenerator extends GeneratorCommand
     /**
      * type can be: BarChart, CardCoun, List, PieChart
      */
-    protected $signature = 'system-analytic:handler {name} {type=BarChart}';
+    protected $signature = 'system-analytic:handler {name} {--type=} {--bar-chart} {--list} {--card-count} {--computed} {--pie-chart}';
 
     protected $description = 'Create the System Analytic Skeleton';
 
@@ -22,9 +23,17 @@ class AnalyticHandlerGenerator extends GeneratorCommand
      */
     protected $hubLocation = null;
 
+    protected static $expectedOptions = [
+        'bar-chart' => 'BarChart',
+        'list' => 'List',
+        'card-count' => 'CardCount',
+        'computed' => 'Computed',
+        'pie-chart' => 'PieChart',
+    ];
+
     protected function getStub()
     {
-        return __DIR__ . "/Stubs/{$this->argument('type')}Handler.php.stub";
+        return __DIR__ . "/Stubs/{$this->detectChartType()}.php.stub";
     }
 
     protected function getDefaultNamespace($rootNamespace)
@@ -32,14 +41,58 @@ class AnalyticHandlerGenerator extends GeneratorCommand
         return $rootNamespace . '\\' . Util::folderFromPath(config('system-analytic.analytic_path')) . '\\' . config('system-analytic.folder_name') . '\Handlers';
     }
 
+    /**
+     * From the provided options, 
+     */
+    private function detectChartType()
+    {
+        $typeOption = $this->option('type');
+        $chartType = null;
+
+        if ($typeOption) {
+            $indexExpectedChartType = array_search($typeOption, array_values(static::$expectedOptions));
+
+            if ($indexExpectedChartType === false) {
+                $this->error(
+                    "the type value must be one of:" . implode(',', array_values(static::$expectedOptions))
+                );
+                exit();
+            }
+
+            $chartType = $typeOption;
+        } else {
+            $inputedOptions = Arr::only($this->options(), array_keys(static::$expectedOptions));
+
+
+            foreach ($inputedOptions as $inputOption => $wasSelected) {
+                if (!$wasSelected) continue;
+
+                $chartType = static::$expectedOptions[$inputOption];
+
+                break;
+            }
+        }
+
+
+        if (!$chartType) {
+            $this->error(
+                "the chart type argument is missing, please add to your command one"
+                    . " of: --bar-chart,--list,--card-count,--computed, --pie-chart  "
+            );
+            exit();
+        }
+
+        return $chartType . 'Handler';
+    }
+
     public function handle()
     {
         parent::handle();
 
-        $this->writeStubToCreatedFile();
+        $this->reformatHandlerClassContent();
     }
 
-    protected function writeStubToCreatedFile()
+    protected function reformatHandlerClassContent()
     {
         // Get the fully qualified class name (FQN)
         $class = $this->qualifyClass($this->getNameInput());
